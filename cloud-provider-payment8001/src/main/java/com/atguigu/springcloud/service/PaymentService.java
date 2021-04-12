fcf18e7469146c38236d9f9b13017c4c2d7b1409
guohaocong <3330159405@qq.com>
2021-04-12 17:30:30 +0800

    cloud2020第一次提交

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -0,0 +1,11 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public interface PaymentService {
+
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@PathVariable Long id);
+}
