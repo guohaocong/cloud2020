324d19a843c1ec15702acf3f284171cd4188eea7
guohaocong <3330159405@qq.com>
2021-04-19 16:17:05 +0800

    cloud2020

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,11 @@
+//import java.time.ZonedDateTime;
+//
+//public class T2 {
+//
+//    public static void main(String[] args) {
+//        ZonedDateTime zbj = ZonedDateTime.now() ; //默认时区
+//        System.out.println(zbj);
+//
+//        //2021-04-14T10:09:32.007+08:00[Asia/Shanghai]
+//    }
+//}
